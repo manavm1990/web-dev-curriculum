840c5c459ff5e32ceed7ad826ba8cdc4ffcfb3a8
Manav Misra <manavm@visionwebsoft.com>
2025-01-15 17:49:12 -0600

    feat: :sparkles: add separators in between sections

--- app/components/app-sidebar.tsx
+++ app/components/app-sidebar.tsx
@@ -12,6 +12,8 @@ import {
 } from '@/components/ui/sidebar'
 import { Link } from 'next-view-transitions'
 import { usePathname } from 'next/navigation'
+import { Fragment } from 'react'
+import { Separator } from './ui/separator'
 
 const NAV_ITEMS = [
   {
@@ -28,27 +30,43 @@ const NAV_ITEMS = [
   { title: '1-2 File Systems', url: '/file-systems' },
 ] as const
 
+function checkForSectionChange(
+  currentItem: (typeof NAV_ITEMS)[number],
+  previousItem?: (typeof NAV_ITEMS)[number],
+) {
+  if (!previousItem) return false
+
+  const currentSection = currentItem.title.split('-')[1]?.slice(0, 1)
+  const previousSection = previousItem.title.split('-')[1]?.slice(0, 1)
+
+  return currentSection !== previousSection
+}
+
 export function AppSidebar() {
   const pathname = usePathname()
 
   return (
+    // TODO: Add collapsible sections when this 💩 gets too long!
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Table of Contents</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              {NAV_ITEMS.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <Link
-                      href={item.url}
-                      className={pathname === item.url ? 'font-black text-sidebar-primary' : ''}
-                    >
-                      {item.title}
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
+              {NAV_ITEMS.map((item, index, items) => (
+                <Fragment key={item.title}>
+                  {index > 0 && checkForSectionChange(item, items[index - 1]) && <Separator />}
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <Link
+                        href={item.url}
+                        className={pathname === item.url ? 'font-black text-sidebar-primary' : ''}
+                      >
+                        {item.title}
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </Fragment>
               ))}
             </SidebarMenu>
           </SidebarGroupContent>
