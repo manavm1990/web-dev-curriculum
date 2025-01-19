1a607d0bdcff607a1405c85bd69ae1a0ab1d045d
Manav Misra <manavm@visionwebsoft.com>
2025-01-18 18:58:42 -0600

    Update the fucking '.lintstagedrc.js'

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,12 +5,6 @@ const buildEslintCommand = (filenames) => {
   return `next lint --fix --file ${files}`
 }
 
-// Only type check the staged files and their related imports
-const buildTypeCheckCommand = (filenames) => {
-  const files = filenames.map((f) => path.relative(process.cwd(), f)).join(' ')
-  return `tsc --noEmit --incremental false ${files}`
-}
-
 module.exports = {
-  '*.{ts,tsx}': [buildEslintCommand, buildTypeCheckCommand, 'prettier --write'],
+  '*.{ts,tsx}': [buildEslintCommand, 'prettier --write'],
 }
