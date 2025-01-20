ee7db10183047316e722b8b48e1b53fa10b6e660
Manav Misra <manavm@visionwebsoft.com>
2025-01-19 19:11:48 -0600

    fix: :lipstick:

--- app/components/ui/sidebar.tsx
+++ app/components/ui/sidebar.tsx
@@ -476,7 +476,7 @@ const SidebarMenu = React.forwardRef<HTMLUListElement, React.ComponentProps<'ul'
     <ul
       ref={ref}
       data-sidebar="menu"
-      className={cn('flex w-full min-w-0 flex-col gap-1', className)}
+      className={cn('flex w-full min-w-0 list-none flex-col gap-1', className)}
       {...props}
     />
   ),
