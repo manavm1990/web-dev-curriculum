e6828cdcd00dbd4d6574d9ed14229dab44a134ae
Manav Misra <manavm@visionwebsoft.com>
2025-01-23 19:33:13 -0600

    :arrow_up: npx @tailwindcss/upgrade@next

--- app/components/ui/typography/p.tsx
+++ app/components/ui/typography/p.tsx
@@ -1,5 +1,5 @@
 import type TypographyProps from './typography.types'
 
 export default function P({ children }: TypographyProps) {
-  return <p className="leading-7 [&:not(:first-child)]:mt-6">{children}</p>
+  return <p className="leading-7 not-first:mt-6">{children}</p>
 }
