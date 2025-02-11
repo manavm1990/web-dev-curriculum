d509f7cca4b948e4c482dd0ccb4cf15e9c7e33d2
Manav Misra <manavm@visionwebsoft.com>
2025-02-11 08:56:56 -0600

    docs: :memo:

--- app/components/ui/typography/headings/h4.tsx
+++ app/components/ui/typography/headings/h4.tsx
@@ -2,13 +2,13 @@ import { Link } from 'next-view-transitions'
 import type HeadingProps from './headings.types'
 import { createHeadingId } from './utils'
 
-export default function H4({ children }: HeadingProps) {
+export default function H4({ children, className }: HeadingProps) {
   const id = createHeadingId(children)
 
   return (
     <Link
       href={`#${id}`}
-      className="my-2 inline-block scroll-m-20 text-xl font-semibold tracking-tight underline underline-offset-2"
+      className={`my-2 inline-block scroll-m-20 text-xl font-semibold tracking-tight underline underline-offset-2 ${className ? className : ''}`}
     >
       <h4 id={id}>{children}</h4>
     </Link>
--- app/components/ui/typography/p.tsx
+++ app/components/ui/typography/p.tsx
@@ -1,5 +1,5 @@
 import type TypographyProps from './typography.types'
 
-export default function P({ children }: TypographyProps) {
-  return <p className="leading-7 not-first:mt-6">{children}</p>
+export default function P({ children, className }: TypographyProps) {
+  return <p className={`leading-7 not-first:mt-6 ${className}`}>{children}</p>
 }
