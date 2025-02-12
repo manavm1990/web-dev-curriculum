7bd80583d7c5ee440892cdc63f4113a5c3894516
Manav Misra <manavm@visionwebsoft.com>
2025-02-12 16:05:42 -0600

    fix: handle optional className prop in typography component

--- app/components/ui/typography/p.tsx
+++ app/components/ui/typography/p.tsx
@@ -1,5 +1,5 @@
 import type TypographyProps from './typography.types'
 
 export default function P({ children, className }: TypographyProps) {
-  return <p className={`leading-7 not-first:mt-6 ${className}`}>{children}</p>
+  return <p className={`leading-7 not-first:mt-6 ${className ? className : ''}`}>{children}</p>
 }
