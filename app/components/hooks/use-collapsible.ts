803d054331f08609374df03d91325520b9edf64a
Manav Misra <manavm@visionwebsoft.com>
2025-03-16 20:44:31 -0500

    feat: :children_crossing:

--- app/components/hooks/use-collapsible.ts
+++ app/components/hooks/use-collapsible.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 export default function useCollapsible() {
-  const [isOpen, setIsOpen] = useState(true)
+  const [isOpen, setIsOpen] = useState(false)
 
   return {
     isOpen,
